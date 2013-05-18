4736ea0fee12c61305891e2df3be20fc636e4986
Alexander Nestorov <alexandernst@gmail.com>
2013-05-18 02:08:37 +0200

    Make this thing run in the background but only when events get fired

--- js/main.js
+++ js/main.js
@@ -3,3 +3,69 @@ chrome.browserAction.onClicked.addListener(function(tab){
 		url: chrome.extension.getURL("panorama.html")
 	});    
 });
+
+//listen for events for new windows/tabs
+//listen for events for closed windows/tabs
+//listen for runtime.onSuspend
+
+chrome.alarms.create("auto-save", {
+	when: 1,
+	periodInMinutes: 1
+});
+
+chrome.alarms.onAlarm.addListener(function(alarm){
+	if(alarm.name == "auto-save"){
+		saveCurrentTabs();
+	}
+});
+
+function saveCurrentTabs(){
+	console.log("save current tabs!");
+
+	chrome.windows.getAll(function(windows){
+
+		var data = [];
+
+		//Get only normal windows
+		windows = _.filter(windows, function(w){
+			return w.type == "normal";
+		});
+
+		//Get tabs of each window
+		_.each(windows, function(w){
+
+			var wdata = [];
+
+			chrome.tabs.query({
+				windowId: w.id
+			}, function(tabs){
+
+				_.each(tabs, function(tab){
+
+					if(tab.url != chrome.extension.getURL("panorama.html")){
+						var tabdata = {};
+						tabdata.pinned = tab.pinned;
+						tabdata.active = tab.active;
+						tabdata.url = tab.url;
+						tabdata.title = tab.title;
+						tabdata.favIconUrl = tab.favIconUrl;
+						tabdata.incognito = tab.incognito;
+						wdata.push(tabdata);
+					}
+
+				});
+
+				data.push(wdata);
+
+				console.log(data);
+				chrome.storage.local.set({
+					'tabs': data
+				});
+
+			});
+
+		});
+
+	});
+
+}
