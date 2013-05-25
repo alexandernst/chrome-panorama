3fdccb4baa726056289b53cb8707db51368df8b7
Alexander Nestorov <alexandernst@gmail.com>
2013-05-25 13:57:25 +0200

    Create WindowModel, TabsCollection and TabModel

--- js/panorama-models.js
+++ js/panorama-models.js
@@ -1,8 +1,17 @@
-var DataModel = Backbone.Model.extend({
+var TabModel = Backbone.Model.extend({
+
+});
+
+var TabsCollection = Backbone.Collection.extend({
+	chromeStorage: new Backbone.ChromeStorage("panorama", "local"),
+	model: TabModel	
+});
+
+var WindowModel = Backbone.Model.extend({
 	initialize: function(){
 		var tabs = this.get("tabs");
 		if(tabs){
-			this.tabs = new PanoramaCollection(tabs);
+			this.tabs = new TabsCollection(tabs);
 			this.unset("tabs");
 		}
 	}
@@ -10,5 +19,5 @@ var DataModel = Backbone.Model.extend({
 
 var PanoramaCollection = Backbone.Collection.extend({
 	chromeStorage: new Backbone.ChromeStorage("panorama", "local"),
-	model: DataModel
+	model: WindowModel
 });
